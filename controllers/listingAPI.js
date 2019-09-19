b69d945b6bf36bfcccbb5f25934c21208023a26a
David De Angelis <daviddeangelis@Davids-MacBook-Pro-2.local>
2019-09-19 10:13:55 +1000

    Model and Controller for Purchase Summary
    Added sql query, and what not for Ghssan's front-end for the Purchase Summary Page (shown after a purchase has been completed)

--- controllers/listingAPI.js
+++ controllers/listingAPI.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const listing = require('../models/listing');
+const listingModel = require('../models/listing');
 const view = require('../views/listingView');
 
 // Every method is prepended with "/listing" see app.js
@@ -8,23 +8,40 @@ const view = require('../views/listingView');
 router.get('/id=:id', (req, res) => // e.g. listing/id=4bb8590e-ce26-11e9-a859-256794b0b57d
 {
 	console.log('Receieved req for listing id: ' + req.params.id); // Example params usage.
-	listing.GetListing(req.params.id, 
+	listingModel.GetListing(req.params.id, 
 		(err, results) =>
 		{ 
 			if (err) throw err;
 			res.send(view.viewListing(results));
 		});
-})
+});
 
 router.get('/search=:query', (req, res) => 
 {
 	console.log('Received search query: ' + req.params.query); // Example params usage.
-	listing.SearchListings(req.params.query, 
+	listingModel.SearchListings(req.params.query, 
 		(err, results) =>
 		{ 
 			if (err) throw err;
 			res.send(view.viewListings(results));
 		});
-})
+});
+
+router.get('/summary=:purchaseID', (req, res) =>
+{
+	console.log('Received request to see purchase summary.')
+	var userPK = "We have to retrieve the user PK from the session"; //and pass it through to the 'GetPurchaseSummary' such that a user cannot see another users purchase summaries
+	listingModel.GetPurchaseSummary(req.params.purchaseID, userPK, 		
+		{
+		found: 
+			(result) => 
+			{
+				console.log(result);
+				res.send(jsonResponse.success());
+			},
+		notFound: 
+			() => res.send(jsonResponse.fail("Payment Summary Not Found")),
+		})
+});
 
 module.exports = { router };
