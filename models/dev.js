5ad3cdd862f4135533cd1fb15838105cbd56d529
David De Angelis <daviddeangelis@Davids-MacBook-Pro-2.local>
2019-09-21 09:52:59 +1000

    Some basic test data everyone can use

--- models/dev.js
+++ models/dev.js
@@ -1,7 +1,12 @@
 const path = require('path');
 const db = require('../utils/database');
 
-exports.createSchema = function(name, password) 
+exports.createSchema = function() 
 {
-	return db.createSchema();
+	db.createSchema();
+}
+
+exports.populateDatabase = function() 
+{
+	db.populateDatabase();
 }
