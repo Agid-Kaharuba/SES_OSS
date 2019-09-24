6972ff9773ee35c0215eeeafc98250d40a106a0b
Agid-Kaharuba <razinagida@gmail.com>
2019-09-24 21:22:06 +1000

    Add admin checking

--- models/admin.js
+++ models/admin.js
@@ -1,3 +1,28 @@
-const mysql = require("mysql");
-const path = require('path');
-const db = require('../utils/database');
+const database = require('../utils/database');
+
+exports.checkAdminPrivileges = function(user, callback = (hasPrivileges) => {}) 
+{
+    const db = database.connectDatabase();
+    let query = 
+    `SELECT * from User INNER JOIN Admin ON Admin.AD_US = User.US_PK 
+    WHERE User.US_PK = ? LIMIT 1`;
+
+    db.query(query, user.id, (err, results) => 
+    {
+        if (results.length > 0)
+            callback(true);
+        else
+            callback(false);
+    })
+}
+
+exports.tryAdminAction = function(user, callback = {success: () => {}, fail: () => {}})
+{
+    checkAdminPrivileges(user, (hasPrivileges) =>
+    {
+        if (hasPrivileges)
+            success();
+        else
+            fail();
+    })
+}
