f014c4ca4693d910da18b6fb85ac6afa4df1fd13
Krystian Huang <krystianhuang@gmail.com>
2019-09-27 20:44:17 +1000

    0. Fixed missing brackets 1. Hide and show (carts, profile, logout) buttons in header. 2. Redirect search bar action to proper url. 3. Redirect cart, profile, logout buttons to proper url.

--- controllers/userAPI.js
+++ controllers/userAPI.js
@@ -8,71 +8,69 @@ const router = express.Router();
 
 // Every method is prepended with "/user" see app.js
 
-router.post('/register', (req, res) =>
-{
-    var user = req.body;
-        userModel.checkUserExists(user.username,
-        {
-            found:
-                () => res.send(jsonResponse.fail("Could not register an already existing user")),
-            notFound:
-                () => userModel.registerUser(user,
-                    {
-                        success:
-                            () => res.redirect("/?register=success"),
-                        fail:
-                            (reason) => res.send(jsonResponse.fail(reason))
-                    })
-        });
+router.post('/register', (req, res) => {
+	var user = req.body;
+	userModel.checkUserExists(user.username,
+		{
+			found:
+				() => res.send(jsonResponse.fail("Could not register an already existing user")),
+			notFound:
+				() => userModel.registerUser(user,
+					{
+						success:
+							() => res.redirect("/?register=success"),
+						fail:
+							(reason) => res.send(jsonResponse.fail(reason))
+					})
+		});
 });
 
-router.post('/login', (req, res) =>
-{
-    console.log(req.body);
-    userModel.loginUser(req.body.username, req.body.password,
-        {
-            success:
-                (user) => auth.attach(res, user,
-                    {
-                        success: () => res.redirect("/?login=success"),
-                        fail: () => res.send(jsonResponse.fail('Failed to create a new session'))
-                    }),
-            fail:
-                (reason) => res.send(jsonResponse.fail(reason)),
-        });
+router.post('/login', (req, res) => {
+	console.log(req.body);
+	userModel.loginUser(req.body.username, req.body.password,
+		{
+			success:
+				(user) => auth.attach(res, user,
+					{
+						success: () => {
+							res.cookie("currentUser", user.username);
+							res.redirect("/?login=success");
+						},
+						fail: () => res.send(jsonResponse.fail('Failed to create a new session'))
+					}),
+			fail:
+				(reason) => res.send(jsonResponse.fail(reason)),
+		});
 });
 
-router.get('/view-account', auth.authorizeUser, (req, res) => 
-{
-    //res.send(userView.viewAccount());
+router.get('/view-account', auth.authorizeUser, (req, res) => {
+	//res.send(userView.viewAccount());
 });
 
-router.post('/logout', (req, res) => 
-{
-    auth.invalidateSession(req, {
-        success: () => res.send('Logout Sucessful'),
-        fail: () => res.send(jsonResponse.fail('Failed to logout'))
-    })
+router.get('/logout', (req, res) => {
+	auth.invalidateSession(req, {
+		success: () => {
+			res.cookie('currentUser', "", {maxAge: Date.now()});
+			res.redirect("/?logout=success");
+		},
+		fail: () => res.send(jsonResponse.fail('Failed to logout'))
+	})
 });
 
-router.put('/modify', (req, res) => 
-{
-
-});
-
-router.get('/purchase/:listingID', (req, res) => 
-{
+router.put('/modify', (req, res) => {
 
 });
 
-router.post('/confirm_purchase/:listingID', (req, res) => 
-{
+router.get('/purchase/:listingID', (req, res) => {
 
 });
 
-router.get('/', (req, res) =>
-{
+router.post('/confirm_purchase/:listingID', (req, res) => {
 
 });
 
-module.exports = { router };
+router.get('/', (req, res) => {
+
+});
+
+module.exports = {router};
